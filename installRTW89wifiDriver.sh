76a9d5c7bd16907cd294f3db014131203ef2a74c
mark <89848850+wottreng@users.noreply.github.com>
2021-11-29 20:24:50 -0500

    Update installRTW89wifiDriver.sh

--- installRTW89wifiDriver.sh
+++ installRTW89wifiDriver.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# READ the DOCS: https://github.com/lwfinger/rtw89
+echo "cloning rtw89 repository..."
 git clone https://github.com/lwfinger/rtw89.git -b v5
 cd rtw89
+echo "build driver..."
 make
+echo "install driver..."
 sudo make install
+echo "activate driver..."
 sudo modprobe rtw89pci
+echo "wifi should work now, Cheers"
